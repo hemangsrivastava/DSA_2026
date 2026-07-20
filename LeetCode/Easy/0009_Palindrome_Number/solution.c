c934ae3c174a44cc864e32efcd006b13f6cb56a3
hemangsrivastava <hemangsrivastava.contact@gmail.com>
2026-07-20 17:00:49 +0530

    Solve LeetCode #9: Palindrome Number

--- LeetCode/Easy/0009_Palindrome_Number/solution.c
+++ LeetCode/Easy/0009_Palindrome_Number/solution.c
@@ -1,1 +1,15 @@
-
+bool isPalindrome(int x) {
+    if (x<0)
+        return false; 
+    int temp;
+    temp=x; 
+    long long rev=0; 
+    while (x>0){
+        rev=rev*10+x%10;
+        x/=10;
+    }
+    if (temp==rev)
+        return true;
+    else
+        return false;
+}
