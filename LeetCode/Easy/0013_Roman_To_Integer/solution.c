bd73c0b23a1ef99da0f472ba87ef17ce34985a34
hemangsrivastava <hemangsrivastava.contact@gmail.com>
2026-07-20 19:02:36 +0530

    Add function to convert Roman numerals to integers
    Implement Roman numeral to integer conversion.

--- LeetCode/Easy/0013_Roman_To_Integer/solution.c
+++ LeetCode/Easy/0013_Roman_To_Integer/solution.c
@@ -1,1 +1,34 @@
+int value(char c){
+    switch(c){
+        case 'I':
+        return 1;
+        case 'V':
+        return 5;
+        case 'X':
+        return 10;
+        case 'L':
+        return 50;
+        case 'C':
+        return 100;
+        case 'D':
+        return 500;
+        case 'M':
+        return 1000;
+        default :
+        return 0;
+    }
+}
+int romanToInt(char *s) {
+    int i,x=0;
+for (i=0;s[i]!='\0';i++){
+    if(value(s[i]) < value(s[i+1])){
+        x=x-(value(s[i]));
+    }
+    else{
+        x=x+(value(s[i]));
+    }
+}
 
+return x;
+    
+}
